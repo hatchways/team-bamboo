54338e51bbc5cdcf6d90a4c0b8723d0899830090
sunmengyue <mengyuesun@gmail.com>
2022-01-12 10:03:54 -0500

    add protectedRoute component

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, RouteProps, useHistory } from 'react-router-dom';
+import { useAuth } from '../../context/useAuthContext';
+import { CircularProgress } from '@mui/material';
+
+const ProtectedRoute: React.FC<RouteProps> = ({ ...rest }) => {
+  const {loggedInUser} = useAuth();
+  const history = useHistory();
+
+  if (loggedInUser === undefined) return <CircularProgress />;
+  if (!loggedInUser) {
+    history.push('/login');
+    // loading for a split seconds until history.push works
+    return <CircularProgress />;
+  }
+  
+  return <Route {...rest}/>
+  
+}
+
+export default ProtectedRoute;
