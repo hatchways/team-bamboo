318cd15f80fe463123994ad46bf286401a364ada
sunmengyue <mengyuesun@gmail.com>
2022-01-14 17:55:49 -0500

    create an interface for ProtectedRouteProps

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -3,11 +3,15 @@ import { Route, RouteProps, useHistory } from 'react-router-dom';
 import { useAuth } from '../../context/useAuthContext';
 import { CircularProgress } from '@mui/material';
 
-const ProtectedRoute = ({ component: Component, ...rest }: RouteProps): JSX.Element => {
+interface ProtectedRouteProps extends Omit<RouteProps, 'component'> {
+  component: React.ElementType;
+}
+
+const ProtectedRoute = ({ component: Component, ...rest }: ProtectedRouteProps): JSX.Element => {
   const { loggedInUser } = useAuth();
   const history = useHistory();
   if (loggedInUser === undefined) return <CircularProgress />;
-  if (!loggedInUser || !Component) {
+  if (!loggedInUser) {
     history.push('/login');
     // loading for a split seconds until history.push works
     return <CircularProgress />;
