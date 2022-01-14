31afe4efa083c271bf094f1e13375db0c63836b1
KookiKodes <kookikodes@gmail.com>
2022-01-13 21:39:15 -0800

    Added additional schema validation

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,35 +1,45 @@
 const mongoose = require("mongoose");
 
+const notifyTypes = ["user", "message"];
+const requireSender = ["user", "message"];
+
 const notificationSchema = new mongoose.Schema(
   {
     notifyType: {
       type: String,
-      enum: ["user", "message"],
-      required: true,
+      enum: notifyTypes,
+      required: [
+        true,
+        `A type of notification is required. Options: ${notifyTypes.join(
+          ", "
+        )}`,
+      ],
     },
     title: {
       type: String,
-      required: true,
+      required: [true, "A notification title is required."],
       trim: true,
       maxlength: 40,
     },
     description: {
       type: String,
-      required: true,
+      required: [true, "A description of the notification is required."],
       trim: true,
       maxLength: 150,
     },
-    readBy: {
-      type: [
-        {
-          readBy: { type: mongoose.Types.ObjectId, ref: "User" },
-          readAt: { type: Date, default: Date.now() },
-        },
-      ],
-      default: [],
-    },
+    readBy: [
+      {
+        readBy: { type: mongoose.Types.ObjectId, ref: "User" },
+        readAt: { type: Date, default: Date.now() },
+      },
+    ],
     sender: {
       type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [
+        requiresSender,
+        "Notification type requires a user as a sender.",
+      ],
     },
     receivers: {
       type: [
@@ -38,12 +48,27 @@ const notificationSchema = new mongoose.Schema(
           ref: "User",
         },
       ],
-      required: true,
+      validate: [
+        validReceivers,
+        "Must contain at least one recipient and/or sender cannot be included within receivers.",
+      ],
     },
   },
   // Use timestamps instead of date -> createdAt and updatedAt
   { timestamps: true }
 );
+
+function requiresSender() {
+  return requireSender.includes(this.notifyType);
+}
+
+function validReceivers(val) {
+  if (val.length < 1) return false;
+  if (requiresSender.call(this)) {
+    return !this.receivers.includes(this.sender);
+  }
+  return true;
+}
 
 module.exports = Notification = mongoose.model(
   "Notification",
