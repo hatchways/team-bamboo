3ed8a51bccfab5ca2d00e9c38ce7c3169780a5a7
KookiKodes <kookikodes@gmail.com>
2022-01-13 21:39:42 -0800

    Added route for creating notifications

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -1,8 +1,44 @@
 const Notification = require("../models/Notification");
+const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 
 exports.createNotification = asyncHandler(async (req, res, next) => {
-  //
+  // received from "protected" middleware function
+  const {
+    user,
+    body: { notifyType, title, description, receivers },
+  } = req;
+
+  const sender = await User.findById(user.id);
+
+  if (!sender) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  const notification = await new Notification({
+    sender: sender.id,
+    notifyType,
+    title,
+    description,
+    receivers,
+  }).save();
+
+  if (notification) {
+    res.status(201).json({
+      success: {
+        id: notification.id,
+        createdAt: notification.createdAt,
+        notifyType: notification.notifyType,
+        title: notification.title,
+        description: notification.description,
+        receivers: notification.receivers,
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid notification data");
+  }
 });
 
 exports.markNotificationRead = asyncHandler(async (req, res, next) => {
