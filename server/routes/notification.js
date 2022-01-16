17950aaff3336547433500c277cd53ebbbc3767e
KookiKodes <kookikodes@gmail.com>
2022-01-16 14:58:50 -0800

    updated routes to follow CRUD guidelines

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -5,14 +5,11 @@ const {
   createNotification,
   markNotificationRead,
   getAllNotifications,
-  markAllNotificationsRead,
 } = require("../controllers/notification");
 
 router.route("/").post(protect, createNotification);
 
 router.route("/mark-read/:id").patch(protect, markNotificationRead);
-
-router.route("/all/mark-read").patch(protect, markAllNotificationsRead);
 
 router.route("/all").get(protect, getAllNotifications);
 
