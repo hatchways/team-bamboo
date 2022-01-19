4555296f078a50a317ffa09f295ce7a03e79cd2b
KookiKodes <kookikodes@gmail.com>
2022-01-19 13:32:12 -0800

    completed popper component

--- client/src/components/UnreadNotifications/UnreadNotificationsPopper.tsx
+++ client/src/components/UnreadNotifications/UnreadNotificationsPopper.tsx
@@ -1,5 +1,8 @@
-import { Stack, Paper, Popper, styled, Box, Fade } from '@mui/material';
+import type { ValidationError, RequestError } from '../../interface/ApiData';
+import type { MouseEvent, ReactElement } from 'react';
+import { Stack, Paper, Popper, Box, Typography, ClickAwayListener, styled, Fade } from '@mui/material';
 import UnreadNotificationSkeleton from './UnreadNotificationSkeleton';
+import UnreadNotificationItem from './UnreadNotificationItem';
 import { useNotifications } from '../../context/useNotificationContext';
 import { useUnreadNotificationsPopper } from './context/useNotificationPopperContext';
 
@@ -16,59 +19,81 @@ const Arrow = styled(Box)({
     position: 'absolute',
     width: 12,
     height: 12,
-    top: -10,
+    top: -6,
     transform: 'rotate(45deg)',
     left: 'calc(50% - 6px)',
     zIndex: -1,
   },
 });
 
+const StyledStack = styled(Stack)(({ theme }) => ({
+  padding: theme.spacing(3),
+  background: theme.palette.background.default,
+  minWidth: 400,
+  maxHeight: 200,
+  overflowY: 'auto',
+}));
+
+const NoNotificationsText = () => (
+  <Typography
+    variant="h6"
+    color="GrayText"
+    textAlign="center"
+    width="100%"
+  >{`You don't have any new notifications.`}</Typography>
+);
+
+const ErrorText = ({ children }: { children: string }) => {
+  return (
+    <Typography width="100%" textAlign="center" color="GrayText">
+      {children}
+    </Typography>
+  );
+};
+
 const UnreadNotificationsPopper = () => {
-  const { matchNotifications, loadNotifications } = useNotifications();
-  const { anchorEl, isOpen } = useUnreadNotificationsPopper();
+  const { matchNotifications } = useNotifications();
+  const { anchorEl, onClose, isOpen } = useUnreadNotificationsPopper();
 
   return (
-    <>
-      <button onClick={loadNotifications} ref={anchorEl}>
-        Notifications
-      </button>
-      <Popper open={true} anchorEl={anchorEl.current} placement="bottom" transition>
+    <ClickAwayListener onClickAway={onClose}>
+      <Popper open={isOpen} anchorEl={anchorEl} placement="bottom" transition>
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
-            <Paper elevation={12} sx={{ overflowY: 'hidden' }}>
-              {/* Figure out the arrow */}
+            <Paper elevation={12}>
               <Arrow />
-              <Stack
-                spacing={3}
-                p={3}
-                bgcolor="white"
-                width="min-content"
-                minWidth={400}
-                maxHeight={200}
-                sx={{ overflowY: 'auto' }}
-              >
-                {matchNotifications(
-                  () => (
-                    <>
-                      <UnreadNotificationSkeleton />
-                      <UnreadNotificationSkeleton />
-                      <UnreadNotificationSkeleton />
-                    </>
-                  ),
-                  (error) => {
-                    console.log(error);
-                    return <p>An error occurred</p>;
-                  },
-                  (data) => (
-                    <></>
-                  ),
-                )}
-              </Stack>
+              <Box sx={{ overflowY: 'hidden' }}>
+                <StyledStack spacing={3}>
+                  {matchNotifications<ReactElement | ReactElement[]>(
+                    () => (
+                      <>
+                        <UnreadNotificationSkeleton />
+                        <UnreadNotificationSkeleton />
+                        <UnreadNotificationSkeleton />
+                      </>
+                    ),
+                    (error) =>
+                      !(error instanceof Array) ? (
+                        <ErrorText>{error.message}</ErrorText>
+                      ) : (
+                        error.map(({ msg }, index) => <ErrorText key={index}>{msg}</ErrorText>)
+                      ),
+                    (data) =>
+                      data.notifications.length === 0 ? (
+                        <NoNotificationsText />
+                      ) : (
+                        data.notifications.map(({ id, receivers, ...notification }) => (
+                          <UnreadNotificationItem key={id} {...notification} />
+                        ))
+                      ),
+                  )}
+                </StyledStack>
+              </Box>
             </Paper>
           </Fade>
         )}
       </Popper>
-    </>
+    </ClickAwayListener>
   );
 };
 
