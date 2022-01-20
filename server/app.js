eabd1792ce54f1d8ca9de4b6c61154a2b2b910b9
michael-levitski <66583830+michael-levitski@users.noreply.github.com>
2022-01-20 13:44:17 -0800

    Revert "Request controller"

--- server/app.js
+++ server/app.js
@@ -11,8 +11,7 @@ const logger = require("morgan");
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
-const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/request");
+const profileRouter = require('./routes/profile');
 
 const { json, urlencoded } = express;
 
@@ -46,7 +45,6 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
-app.use("/requests", requestRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
