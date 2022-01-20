60c50f414065f5181cf77fc4c580139e16064c0e
KookiKodes <kookikodes@gmail.com>
2022-01-19 17:44:51 -0800

    wrapped notification items in a link

--- client/src/components/UnreadNotifications/UnreadNotificationItem.tsx
+++ client/src/components/UnreadNotifications/UnreadNotificationItem.tsx
@@ -1,38 +1,46 @@
 import type { Notification } from '../../interface/Notification';
-import { Box, Avatar, Stack, Typography } from '@mui/material';
+import { Box, Avatar, Stack, Typography, styled } from '@mui/material';
 import moment from 'moment';
+import { NavLink } from 'react-router-dom';
 
 type Props = Omit<Notification, 'receivers' | 'id'>;
+
+const Link = styled(NavLink)({
+  textDecoration: 'none',
+});
 
 // Will use notifyType later when we need to style notifications differently based on their type.
 const NotificationItem = ({ notifyType, title, description, sender, createdAt }: Props) => {
   return (
-    <Stack direction="row" spacing={2}>
-      <Avatar
-        alt={sender?.name || 'Robot'}
-        src={sender?.photo || `https://robohash.org/${description}.png`}
-        sx={{ width: 54, height: 54 }}
-        variant="square"
-      />
-      <Stack maxHeight={54} maxWidth={396} overflow="hidden" spacing={0.5}>
-        <Box>
-          <Typography
-            m={0}
-            lineHeight="1rem"
-            variant="h6"
-            sx={{ textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}
-          >
-            {description}
+    //Will need to update notification route in the future based off of the type of notification
+    <Link to="#notification">
+      <Stack direction="row" spacing={2}>
+        <Avatar
+          alt={sender?.name || 'Robot'}
+          src={sender?.photo || `https://robohash.org/${description}.png`}
+          sx={{ width: 54, height: 54 }}
+          variant="square"
+        />
+        <Stack maxHeight={54} maxWidth={396} overflow="hidden" spacing={0.5}>
+          <Box>
+            <Typography
+              m={0}
+              lineHeight="1rem"
+              variant="h6"
+              sx={{ textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' }}
+            >
+              {description}
+            </Typography>
+            <Typography color="GrayText" variant="caption">
+              {title}
+            </Typography>
+          </Box>
+          <Typography variant="body2" fontWeight={500}>
+            {moment(createdAt).format('MM/DD/YYYY')}
           </Typography>
-          <Typography color="GrayText" variant="caption">
-            {title}
-          </Typography>
-        </Box>
-        <Typography variant="body2" fontWeight={500}>
-          {moment(createdAt).format('MM/DD/YYYY')}
-        </Typography>
+        </Stack>
       </Stack>
-    </Stack>
+    </Link>
   );
 };
 
