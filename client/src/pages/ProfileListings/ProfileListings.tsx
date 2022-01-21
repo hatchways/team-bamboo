7a4e489b28943beeabbbc562adaf1df721db7ea0
KookiKodes <kookikodes@gmail.com>
2022-01-21 11:20:45 -0800

    initial commit

---
+++ client/src/pages/ProfileListings/ProfileListings.tsx
@@ -0,0 +1,14 @@
+import PageContainer from '../../components/PageContainer/PageContainer';
+
+const ProfileListings = () => {
+  return (
+    <PageContainer>
+      <h1>Your search results</h1>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </PageContainer>
+  );
+};
+
+export default ProfileListings;
