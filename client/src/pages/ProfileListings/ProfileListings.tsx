07ee0c230e8f692f3df046fafa8edf31b581ebe4
KookiKodes <kookikodes@gmail.com>
2022-01-21 16:12:34 -0800

    Created listings page and removed unused file

--- client/src/pages/ProfileListings/ProfileListings.tsx
+++ client/src/pages/ProfileListings/ProfileListings.tsx
@@ -1,12 +1,32 @@
+import { Typography, Stack, Grid, styled } from '@mui/material';
 import PageContainer from '../../components/PageContainer/PageContainer';
+import SearchBar from './SearchBar/SearchBar';
+import { ProfileCard } from '../../components/ProfileCard';
+
+const Title = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h3,
+  textAlign: 'center',
+  fontWeight: 600,
+}));
 
 const ProfileListings = () => {
   return (
     <PageContainer>
-      <h1>Your search results</h1>
-      {/*  */}
-      {/*  */}
-      {/*  */}
+      <Stack py={4} px={8} gap={6}>
+        <Title>Your search results</Title>
+        <SearchBar />
+        <Grid container spacing={10}>
+          <Grid item xs={4}>
+            <ProfileCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProfileCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProfileCard />
+          </Grid>
+        </Grid>
+      </Stack>
     </PageContainer>
   );
 };
