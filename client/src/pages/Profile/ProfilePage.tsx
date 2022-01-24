f2c0dc08c1d8d2654352b71f3bb51830046b710c
michael-levitski <levitski.michael@gmail.com>
2022-01-24 00:43:06 -0800

    rename Profile page component to ProfilePage

--- client/src/pages/Profile/ProfilePage.tsx
+++ client/src/pages/Profile/ProfilePage.tsx
@@ -2,6 +2,7 @@ import { CircularProgress, Grid } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
+import RequestForm from '../../components/RequestForm/RequestForm';
 import { useAuth } from '../../context/useAuthContext';
 import { Profile } from '../../interface/Profile';
 
@@ -10,7 +11,7 @@ const profile: Profile = {
   availability: [true, false, false, false, false, false, true],
   coverPhoto: 'https://www.kimballstock.com/images/dog-stock-photos.jpg',
   description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex reprehenderit sequi labore atque corporis officia. Ducimus veritatis dolore fugit est, magni alias, iure voluptates quos aliquid sunt sed quaerat! Quos porro cupiditate eum ipsam quam enim odio fugit nam vel! Tempore ut tempora iste laudantium rem dignissimos iure voluptatibus.',
+    'Animals are my passion! I will look after your pets with loving care. I have some availability for pet care in my home as well. I have 10 yrs experience at the Animal Hospital, and have owned multiple pets for many years, including numerous rescues. Kindly email, text or call me and I will respond promptly!',
   hourlyRate: 5,
   name: 'Norma Byers',
   photo: 'https://thoughtcatalog.com/wp-content/uploads/2016/04/24597829769_1c5fd5517a_k.jpg',
@@ -18,6 +19,7 @@ const profile: Profile = {
     'https://i.picsum.photos/id/1062/5092/3395.jpg?hmac=o9m7qeU51uOLfXvepXcTrk2ZPiSBJEkiiOp-Qvxja-k',
     'https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y',
   ],
+  averageRating: 3,
 };
 
 const ProfilePage = () => {
@@ -31,8 +33,9 @@ const ProfilePage = () => {
   // }
   return (
     <PageContainer>
-      <Grid container>
+      <Grid container justifyContent="space-evenly" alignItems="flex-start">
         <ProfileCard profile={profile} />
+        <RequestForm profile={profile} />
       </Grid>
     </PageContainer>
   );
