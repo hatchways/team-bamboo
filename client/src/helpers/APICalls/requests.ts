e6a1e2da3f2c76cfe5dbcdbec68278532761d794
michael-levitski <levitski.michael@gmail.com>
2022-01-26 13:27:13 -0800

    create api call to PATCH /requests

--- client/src/helpers/APICalls/requests.ts
+++ client/src/helpers/APICalls/requests.ts
@@ -1,4 +1,5 @@
 import { Booking } from '../../interface/Booking';
+import { FetchOptions } from '../../interface/FetchOptions';
 
 interface BookingResponse {
   success?: {
@@ -7,8 +8,25 @@ interface BookingResponse {
   error?: string;
 }
 
-export const getBookings = async (sitter = false): Promise<BookingResponse> => {
-  const url = '/requests' + (sitter ? '?isSitter=true' : '');
-  const response = await fetch(url);
+interface SingleBookingResponse {
+  success?: {
+    requests: Booking;
+  };
+  error?: string;
+}
+
+export const getBookings = async (): Promise<BookingResponse> => {
+  const response = await fetch('/requests');
+  return response.json();
+};
+
+export const updateBooking = async (booking: Pick<Booking, 'status'>): Promise<SingleBookingResponse> => {
+  const fetchOptions: FetchOptions = {
+    credentials: 'include',
+    method: 'PATCH',
+    body: JSON.stringify(booking),
+    headers: { 'Content-Type': 'application/json' },
+  };
+  const response = await fetch('/requests', fetchOptions);
   return response.json();
 };
