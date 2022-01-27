362b306f314f7c0d520461a6cf774eb82acfac1c
michael-levitski <levitski.michael@gmail.com>
2022-01-27 01:24:57 -0800

    move booking state into context

--- client/src/pages/Bookings/Bookings.tsx
+++ client/src/pages/Bookings/Bookings.tsx
@@ -6,45 +6,25 @@ import PageContainer from '../../components/PageContainer/PageContainer';
 import NextBooking from '../../components/NextBookings/NextBooking';
 import { Booking } from '../../interface/Booking';
 import BookingList from '../../components/BookingList/BookingList';
-import { getBookings } from '../../helpers/APICalls/requests';
-import { useState, useEffect, useCallback } from 'react';
 import { useSnackBar } from '../../context/useSnackbarContext';
+import { useBookings } from '../../context/useBookingsContext';
 
 const Bookings = () => {
   const { loggedInUser } = useAuth();
   const history = useHistory();
-  const [bookings, setBookings] = useState<Booking[]>([]);
+  const { bookings } = useBookings();
   const { updateSnackBarMessage } = useSnackBar();
-
-  const fetchBookings = useCallback(async () => {
-    try {
-      const response = await getBookings();
-      const { success } = response;
-      if (success) {
-        success.requests.forEach((request) => {
-          const { start, end } = request;
-          request.start = new Date(start);
-          request.end = new Date(end);
-        });
-        setBookings(success.requests);
-      } else {
-        updateSnackBarMessage('Error fetching bookings from server');
-      }
-    } catch (e) {
-      const { message } = e as Error;
-      updateSnackBarMessage(message);
-    }
-  }, [updateSnackBarMessage]);
-
-  useEffect(() => {
-    fetchBookings();
-  }, [fetchBookings]);
-
-  if (loggedInUser === undefined || !bookings) return <CircularProgress />;
+  if (loggedInUser === undefined || bookings === undefined) return <CircularProgress />;
   if (!loggedInUser) {
     history.push('/login');
     return <CircularProgress />;
   }
+
+  if (!bookings) {
+    updateSnackBarMessage('Error fetching bookings');
+    return <></>;
+  }
+
   const highlightedDates = bookings.filter((b) => b.status !== 'declined').map((b) => b.start.toString().slice(0, 10));
 
   const currentBookings: Booking[] = [];
@@ -60,6 +40,7 @@ const Bookings = () => {
 
   const scrollableCardStyles = {
     padding: '0px 10px 10px 10px',
+    minWidth: '280px',
     height: '300px',
     overflow: 'auto',
     '::-webkit-scrollbar': {
