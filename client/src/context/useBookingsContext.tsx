a87589502b1b162a4d87f6dcc9e6d8ce67c489e2
michael-levitski <levitski.michael@gmail.com>
2022-01-27 01:38:28 -0800

    complete booking context

--- client/src/context/useBookingsContext.tsx
+++ client/src/context/useBookingsContext.tsx
@@ -1,10 +1,10 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { getBookings, updateBooking } from '../helpers/APICalls/requests';
-import { Booking } from '../interface/Booking';
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from 'react';
+import { getBookings, updateBooking, SingleBookingResponse } from '../helpers/APICalls/requests';
+import { Booking, Status } from '../interface/Booking';
 
 interface BookingsContext {
-  bookings: Booking[];
-  updateBooking: () => null;
+  bookings: Booking[] | null | undefined;
+  setBookingStatus: (id: string, status: Status) => Promise<SingleBookingResponse>;
 }
 
 interface ProviderPropTypes {
@@ -12,15 +12,49 @@ interface ProviderPropTypes {
 }
 
 const BookingsContext = createContext<BookingsContext>({
-  bookings: [],
-  updateBooking: () => null,
+  bookings: undefined,
+  setBookingStatus: async (): Promise<SingleBookingResponse> => ({}),
 });
 
 export const BookingsProvider = ({ children }: ProviderPropTypes) => {
-  const [bookings, setBookings] = useState([]);
-  return (
-    <BookingsContext.Provider value={{ bookings, updateBooking: () => null }}>{children}</BookingsContext.Provider>
-  );
+  const [bookings, setBookings] = useState<Booking[] | null | undefined>(undefined);
+  const fetchBookings = useCallback(async () => {
+    try {
+      const response = await getBookings();
+      const { success } = response;
+      if (success) {
+        success.requests.forEach((request) => {
+          const { start, end } = request;
+          request.start = new Date(start);
+          request.end = new Date(end);
+        });
+        setBookings(success.requests);
+      } else {
+        setBookings(null);
+      }
+    } catch (e) {
+      setBookings(null);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchBookings();
+  }, [fetchBookings]);
+
+  const setBookingStatus = useCallback(async (id: string, status: Status) => {
+    const booking = await updateBooking({ status }, id);
+    setBookings((prevBookings) => {
+      if (!prevBookings) return prevBookings;
+      const bookingsCopy = [...prevBookings];
+      const updatedBooking = bookingsCopy.find((b) => b._id === id);
+      if (!updatedBooking) return prevBookings;
+      updatedBooking.status = status;
+      return bookingsCopy;
+    });
+    return booking;
+  }, []);
+
+  return <BookingsContext.Provider value={{ bookings, setBookingStatus }}>{children}</BookingsContext.Provider>;
 };
 
 export const useBookings = (): BookingsContext => useContext(BookingsContext);
