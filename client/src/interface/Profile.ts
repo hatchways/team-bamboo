5de50347c972023dead04d075c73f9162af9d757
KookiKodes <kookikodes@gmail.com>
2022-01-26 22:06:17 -0800

    Created Additional types to define new route structure

--- client/src/interface/Profile.ts
+++ client/src/interface/Profile.ts
@@ -8,9 +8,25 @@ export interface Profile {
   gender?: 'male' | 'female' | 'other';
   address?: string;
   telephone?: string;
-  birthday?: string;
+  birthday: string | null;
   photo?: string;
   hourlyRate?: number;
   uploadedImages?: string[];
   averageRating?: number;
+}
+
+export interface SitterProfile extends Profile {
+  hourlyRate: number;
+}
+export interface GetSitterProfileParams {
+  limit?: number;
+  page?: number;
+  order?: 'asc' | 'desc';
+  sort?: keyof Profile;
+  address?: string;
+  start?: string;
+  end?: string;
+}
+export interface GetSitterProfiles {
+  profiles: SitterProfile[];
 }
