3ee503f59c0856886b078a37ff5dc44c4e649010
michael-levitski <levitski.michael@gmail.com>
2022-01-27 01:37:47 -0800

    change sitter to otherUser

--- client/src/components/NextBookings/NextBooking.tsx
+++ client/src/components/NextBookings/NextBooking.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
 import format from 'date-fns/format';
 import { Booking } from '../../interface/Booking';
 import HandleRequestButton from '../HandleRequestButton/HandleRequestButton';
@@ -9,19 +9,19 @@ interface PropTypes {
 
 const NextBooking = ({ booking }: PropTypes) => {
   return (
-    <Card sx={{ padding: '10px 0px 30px 20px' }}>
+    <Card sx={{ padding: '10px 0px 30px 20px', minWidth: '280px' }}>
       <CardContent>
         <Grid container direction="row" justifyContent="space-between" alignItems="flex-end">
           <Typography sx={{ fontVariant: 'small-caps', fontWeight: 'bold' }}>your next booking:</Typography>
-          <HandleRequestButton />
+          <HandleRequestButton bookingId={booking._id} />
         </Grid>
         <Typography sx={{ margin: '10px 0px' }}>
           {format(booking.start, 'd LLLL y, haaa')} - {format(booking.end, 'haaa')}
         </Typography>
         <Grid container direction="row" alignItems="center">
-          <Avatar src={`https://robohash.org/${booking.sitter.email}`}>{booking.sitter.name[0]}</Avatar>
+          <Avatar src={`https://robohash.org/${booking.otherUser.email}`}>{booking.otherUser.name[0]}</Avatar>
           <Typography sx={{ fontSize: '0.9rem', fontWeight: 'bold', marginLeft: '5px' }}>
-            {booking.sitter.name}
+            {booking.otherUser.name}
           </Typography>
         </Grid>
       </CardContent>
