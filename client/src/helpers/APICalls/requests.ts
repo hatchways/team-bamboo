d7075d45f863e39acf14e8f94fadb96c062f8b0a
michael-levitski <levitski.michael@gmail.com>
2022-01-27 01:26:29 -0800

    fix updateBooking()

--- client/src/helpers/APICalls/requests.ts
+++ client/src/helpers/APICalls/requests.ts
@@ -8,7 +8,7 @@ interface BookingResponse {
   error?: string;
 }
 
-interface SingleBookingResponse {
+export interface SingleBookingResponse {
   success?: {
     requests: Booking;
   };
@@ -20,13 +20,14 @@ export const getBookings = async (): Promise<BookingResponse> => {
   return response.json();
 };
 
-export const updateBooking = async (booking: Pick<Booking, 'status'>): Promise<SingleBookingResponse> => {
+export const updateBooking = async (booking: Pick<Booking, 'status'>, id: string): Promise<SingleBookingResponse> => {
   const fetchOptions: FetchOptions = {
     credentials: 'include',
     method: 'PATCH',
     body: JSON.stringify(booking),
     headers: { 'Content-Type': 'application/json' },
   };
-  const response = await fetch('/requests', fetchOptions);
+  const url = '/requests/' + id;
+  const response = await fetch(url, fetchOptions);
   return response.json();
 };
