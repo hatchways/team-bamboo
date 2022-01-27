77f2645fdcc2f47acadd7dd4fe534cad661b0b31
KookiKodes <kookikodes@gmail.com>
2022-01-26 22:08:54 -0800

    Updated to receive query params and add additional validation

--- client/src/pages/ProfileListings/SearchBar/SearchBar.tsx
+++ client/src/pages/ProfileListings/SearchBar/SearchBar.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { KeyboardEvent, useState, FormEvent } from 'react';
 import { IconButton, Grid, InputAdornment, TextField, styled } from '@mui/material';
-import { LocalizationProvider, DatePicker } from '@mui/lab';
+import { LocalizationProvider, DateRangePicker } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import useStyles from './useStyles';
 import { Search, Close } from '@mui/icons-material';
@@ -19,38 +19,75 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 }));
 
 interface State {
-  date: Date | null;
+  start: Date | null;
+  end: Date | null;
+  location: string;
+}
+
+interface Props {
+  start: string | null;
+  end: string | null;
   location: string | null;
 }
 
-const initialState: State = {
-  date: new Date(Date.now()),
-  location: null,
+const initialState = {
+  start: new Date(Date.now()),
+  end: new Date(Date.now()),
+  location: '',
+} as const;
+
+const formatInitialState = (from: string | null, to: string | null, location: string | null) => {
+  const curDate = new Date(Date.now()),
+    dateFrom = from ? new Date(parseInt(from)) : initialState.start,
+    dateTo = to ? new Date(parseInt(to)) : initialState.end,
+    start = dateFrom.getTime() >= curDate.getTime() ? dateFrom : curDate,
+    end = dateTo.getTime() >= curDate.getTime() && dateTo.getTime() >= start.getTime() ? dateTo : start;
+
+  return {
+    location: location ? location : initialState.location,
+    start,
+    end,
+  };
 };
 
-const SearchBar = () => {
+const SearchBar = ({ location, start, end }: Props) => {
   const classes = useStyles();
-  const [value, setValue] = useState<State>(initialState);
+  const curDate = new Date(Date.now());
+  const [value, setValue] = useState<State>(formatInitialState(start, end, location));
 
   const handleChange =
     <T,>(name: keyof State) =>
     (value: T) =>
       setValue((values) => ({ ...values, [name]: value }));
 
+  const handleDateChange = ([start, end]: [Date | null, Date | null]) =>
+    setValue((values) => ({ ...values, start, end }));
+
+  const handleReset = () => setValue((values) => ({ ...values, start: curDate, end: curDate }));
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  const onEnterPress = (event: KeyboardEvent<HTMLFormElement>) => (event.key === 'Enter' ? handleSubmit(event) : null);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} dateFormat="dd MMM yy">
-      <form>
+      <form onSubmit={handleSubmit} onKeyDown={onEnterPress}>
         <Grid container justifyContent="center">
           <Grid container item md={8} xs={12}>
             <Grid item md={7} xs={6}>
               <StyledTextField
                 id="location"
+                value={value.location}
                 fullWidth
                 margin="normal"
                 name="location"
                 placeholder="Location"
                 autoFocus
                 className={classes.left}
+                onChange={(event) => handleChange('location')(event.target.value)}
                 InputProps={{
                   startAdornment: (
                     <label htmlFor="location">
@@ -63,26 +100,29 @@ const SearchBar = () => {
               />
             </Grid>
             <Grid item md={5} xs={6}>
-              <DatePicker
-                value={value.date}
-                onChange={handleChange<Date | null>('date')}
+              <DateRangePicker
+                value={[value.start, value.end]}
+                onChange={handleDateChange}
                 minDate={new Date(Date.now())}
                 OpenPickerButtonProps={{ sx: { ml: 0, mr: 0.5 } }}
-                renderInput={(params) => (
+                renderInput={(startProps) => (
                   <StyledTextField
-                    {...params}
+                    {...startProps}
                     placeholder="DD-DD MMM yy"
                     fullWidth
                     className={classes.right}
+                    label=""
                     inputProps={{
-                      ...params.inputProps,
-                      value: moment(value.date).format('DD MMM yyyy'),
+                      ...startProps.inputProps,
+                      value: [moment(value.start).format('DD MMM'), moment(value.end).format('DD MMM yyyy')].join(
+                        ' - ',
+                      ),
                     }}
                     InputProps={{
-                      startAdornment: params.InputProps?.endAdornment,
+                      startAdornment: startProps.InputProps?.endAdornment,
                       endAdornment: (
                         <InputAdornment position="end">
-                          <IconButton onClick={() => handleChange<Date | null>('date')(new Date(Date.now()))}>
+                          <IconButton onClick={handleReset}>
                             <Close color="inherit" fontSize="medium" />
                           </IconButton>
                         </InputAdornment>
