1e3bd18cd0463d9087d822e9a3c913eec0197a61
sunmengyue <mengyuesun@gmail.com>
2022-01-30 21:55:33 -0500

    Save one weekly schedule in one document

--- server/models/Availability.js
+++ server/models/Availability.js
@@ -1,59 +1,63 @@
 const mongoose = require("mongoose");
 
+function formatDate(time) {
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  return new Date(0, 0, 0, hour, min, 0);
+}
+
+const availableDaySchema = new mongoose.Schema({
+  start: {
+    type: Date,
+    set: formatDate
+  },
+  end: {
+    type: Date,
+    set: formatDate,
+    validate: {
+      validator: function (endTime) {
+        return endTime > this.start;
+      }
+    }
+  }
+});
+
+const availableWeekSchema = new mongoose.Schema({
+  mon: {
+    availableTime: availableDaySchema
+  },
+  tue: {
+    availableTime: availableDaySchema
+  },
+  wed: {
+    availableTime: availableDaySchema
+  },
+  thu: {
+    availableTime: availableDaySchema
+  },
+  fri: {
+    availableTime: availableDaySchema
+  },
+  sat: {
+    availableTime: availableDaySchema
+  },
+  sun: {
+    availableTime: availableDaySchema
+  }
+});
+
 const availabilitySchema = new mongoose.Schema({
   sitterId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "User"
   },
-  availableTime: [
-    {
-      day: {
-        type: Date,
-        required: true
-      },
-      start: {
-        type: Date,
-        required: true
-      },
-      end: {
-        type: Date,
-        required: true,
-        validate: {
-          validator: function (endTime) {
-            return endTime > this.start;
-          }
-        }
-      }
-    }
-  ]
-});
-
-availabilitySchema.virtual("weekDay").get(function () {
-  return this.availableTime.map((item) => {
-    const preFormat = new Date(item.day);
-    const formattedDay = preFormat.toLocaleString(navigator.language, {
-      weekday: "short"
-    });
-    return formattedDay;
-  });
-});
-
-availabilitySchema.virtual("startTime").get(function () {
-  this.availabil;
-  const preFormat = new Date(parseInt(this.availableTime.start));
-  return preFormat.toLocaleTimeString(navigator.language, {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-});
-
-availabilitySchema.virtual("endTime").get(function () {
-  const preFormat = new Date(parseInt(this.availableTime.end));
-  return preFormat.toLocaleTimeString(navigator.language, {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  availability: availableWeekSchema
 });
 
 module.exports = Avalability = mongoose.model(
