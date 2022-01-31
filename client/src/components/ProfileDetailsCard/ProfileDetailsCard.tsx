3b21aabdbf52d495fcb9e8d9bb7b498ae80ef15b
michael-levitski <levitski.michael@gmail.com>
2022-01-31 14:01:18 -0800

    add placeholder data

--- client/src/components/ProfileDetailsCard/ProfileDetailsCard.tsx
+++ client/src/components/ProfileDetailsCard/ProfileDetailsCard.tsx
@@ -7,7 +7,16 @@ interface PropTypes {
   profile: Profile;
 }
 
+const placeholderCoverPhoto = 'https://www.kimballstock.com/images/dog-stock-photos.jpg';
+const placeholderGallery = [
+  'https://i.picsum.photos/id/1062/5092/3395.jpg?hmac=o9m7qeU51uOLfXvepXcTrk2ZPiSBJEkiiOp-Qvxja-k',
+  'https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y',
+];
+
 const ProfileDetailsCard = ({ profile }: PropTypes) => {
+  profile.coverPhoto = placeholderCoverPhoto;
+  profile.uploadedImages = placeholderGallery;
+
   return (
     <Card sx={{ width: '50%' }}>
       <CardMedia image={profile.coverPhoto} title="cover photo" sx={{ minHeight: '200px' }} />
@@ -21,9 +30,10 @@ const ProfileDetailsCard = ({ profile }: PropTypes) => {
         >
           <Avatar sx={{ border: '4px solid white', width: '170px', height: '170px' }} src={profile.photo} />
           <Typography variant="h5">{profile.name}</Typography>
+          <Typography>{profile.jobTitle}</Typography>
           <Typography>
             <Room sx={{ margin: '0rem 0.3rem', color: 'primary.main' }} />
-            {profile.city}
+            {profile.address}
           </Typography>
           <Typography variant="h6" sx={{ width: '100%' }}>
             About me
