4e0c128572b4e67999fa0d266890e3383ba085ce
michael-levitski <levitski.michael@gmail.com>
2022-01-31 14:05:41 -0800

    make profile page dynamic based on id

--- client/src/interface/Profile.ts
+++ client/src/interface/Profile.ts
@@ -1,11 +1,13 @@
 export interface Profile {
+  _id: string;
   coverPhoto: string;
   photo: string;
   uploadedImages: string[];
   name: string;
   description: string;
   hourlyRate: number;
-  availability: boolean[];
-  city: string;
+  address: string;
   averageRating: number;
+  jobTitle: string;
+  userId: string;
 }
