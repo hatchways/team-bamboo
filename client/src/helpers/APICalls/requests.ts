60f8311cd36b45214cf54a6b9c0ded4bca388424
michael-levitski <levitski.michael@gmail.com>
2022-01-31 14:04:20 -0800

    make createBooking() api call

--- client/src/helpers/APICalls/requests.ts
+++ client/src/helpers/APICalls/requests.ts
@@ -1,4 +1,5 @@
 import { Booking } from '../../interface/Booking';
+import { FetchOptions } from '../../interface/FetchOptions';
 
 interface BookingResponse {
   success?: {
@@ -7,8 +8,32 @@ interface BookingResponse {
   error?: string;
 }
 
+export interface SingleBookingResponse {
+  success?: {
+    requests: Booking;
+  };
+  error?: string;
+}
+
+export interface PostBody {
+  sitter: string;
+  start: Date;
+  end: Date;
+}
+
 export const getBookings = async (sitter = false): Promise<BookingResponse> => {
   const url = '/requests' + (sitter ? '?isSitter=true' : '');
   const response = await fetch(url);
+  return response.json();
+};
+
+export const createBooking = async (body: PostBody): Promise<SingleBookingResponse> => {
+  const fetchOptions: FetchOptions = {
+    credentials: 'include',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  };
+  const response = await fetch('/requests', fetchOptions);
   return response.json();
 };
