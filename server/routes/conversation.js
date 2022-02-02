9a964e9c1b1dcc2b4801858b47ef3b15f6f274c5
KookiKodes <kookikodes@gmail.com>
2022-02-02 01:09:05 -0800

    added new route for creation of a conversation.  And added message routes.

--- server/routes/conversation.js
+++ server/routes/conversation.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const { getAllConversations } = require("../controllers/conversation");
+const {
+  getAllConversations,
+  createNewConversation,
+} = require("../controllers/conversation");
+const { getAllMessages, sendMessage } = require("../controllers/message");
+const {
+  validateConversationId,
+  validateMessageData,
+  validateGetMessagesQuery,
+  validateOtherUserId,
+} = require("../validate");
 
 router.route("/").get(protect, getAllConversations);
+router.route("/").post(protect, validateOtherUserId, createNewConversation);
+
+router
+  .route("/:id/messages")
+  .get(protect, validateGetMessagesQuery, getAllMessages);
+router.route("/:id/messages").post(protect, validateMessageData, sendMessage);
 
 module.exports = router;
