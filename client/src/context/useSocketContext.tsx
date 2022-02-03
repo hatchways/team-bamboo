3e9b7e4a372f6115e5efc3382b5e3548580a2704
KookiKodes <kookikodes@gmail.com>
2022-02-02 19:12:56 -0800

    added auth provider requirement. Now handles connection and disconnection of socket

--- client/src/context/useSocketContext.tsx
+++ client/src/context/useSocketContext.tsx
@@ -1,29 +1,49 @@
-import { useState, useContext, createContext, FunctionComponent, useCallback } from 'react';
+import { useState, useContext, createContext, FunctionComponent, useCallback, useEffect } from 'react';
 import { io, Socket } from 'socket.io-client';
+import { useAuth } from './useAuthContext';
 
 interface ISocketContext {
   socket: Socket | undefined;
   initSocket: () => void;
+  disconnectSocket: () => void;
 }
 
 export const SocketContext = createContext<ISocketContext>({
   socket: undefined,
   initSocket: () => null,
+  disconnectSocket: () => null,
 });
 
 export const SocketProvider: FunctionComponent = ({ children }): JSX.Element => {
   const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const { loggedInUser } = useAuth();
 
   const initSocket = useCallback(() => {
-    console.log('trying to connect');
-    setSocket(
-      io('/', {
-        withCredentials: true,
-      }),
-    );
-  }, []);
+    if (!socket) {
+      setSocket(
+        io('/', {
+          withCredentials: true,
+        }),
+      );
+    }
+  }, [socket]);
 
-  return <SocketContext.Provider value={{ socket, initSocket }}>{children}</SocketContext.Provider>;
+  const disconnectSocket = useCallback(() => {
+    if (socket) {
+      socket.disconnect();
+      setSocket(undefined);
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    if (loggedInUser) {
+      initSocket();
+    } else if (!loggedInUser) {
+      disconnectSocket();
+    }
+  }, [initSocket, disconnectSocket, loggedInUser]);
+
+  return <SocketContext.Provider value={{ socket, initSocket, disconnectSocket }}>{children}</SocketContext.Provider>;
 };
 
 export function useSocket(): ISocketContext {
