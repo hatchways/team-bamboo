2a2ea0c6de5b2d886ab803fbbaf8b960d6eee241
KookiKodes <kookikodes@gmail.com>
2022-02-04 13:29:17 -0800

    Display snackbar error on catch error

--- client/src/pages/ProfileListings/hooks/useSitterProfiles.ts
+++ client/src/pages/ProfileListings/hooks/useSitterProfiles.ts
@@ -4,26 +4,28 @@ import { useEffect, useState, useCallback, useRef } from 'react';
 import { v4 as uuid } from 'uuid';
 import { getSitterProfiles } from '../../../helpers/APICalls/profile';
 import wait from '../../../helpers/wait';
+import { useSnackBar } from '../../../context/useSnackbarContext';
 
 interface Options extends Omit<GetSitterProfileParams, 'page'> {
   delay?: number;
   loadOnMount?: boolean;
 }
 
-interface StateItem {
+interface ProfileCardItem {
   id: string;
   loading: boolean;
   sitter: SitterProfile | null;
 }
 
-type OnSuccess<R> = (profiles: StateItem[]) => R;
+type OnSuccess<R> = (profiles: ProfileCardItem[]) => R;
 type MatchProfiles = <R>(onError: OnError<R>, onSuccess: OnSuccess<R>) => R;
 
 const useSitterProfiles = (
   { delay = 500, loadOnMount = true, limit = 6, ...initialParams }: Options = { delay: 50000, limit: 6 },
 ) => {
   const [error, setError] = useState<ValidationError[] | RequestError | null>(null);
-  const [profiles, setProfiles] = useState<StateItem[]>([]);
+  const [profiles, setProfiles] = useState<ProfileCardItem[]>([]);
+  const { updateSnackBarMessage } = useSnackBar();
   const isLoading = useRef<boolean>(loadOnMount);
   const firstLoad = useRef<boolean>(true);
   const page = useRef<number>(1);
@@ -61,8 +63,13 @@ const useSitterProfiles = (
       if (res.error) setError(res.error);
       if (res.errors) setError(res.errors);
       wait(() => setLoadingProfiles(false, res?.success?.profiles), delay);
-    } catch (err) {}
-  }, [limit, page, initialParams, setLoadingProfiles, delay]);
+    } catch (err) {
+      if (err instanceof Error) {
+        return updateSnackBarMessage(err.message);
+      }
+      return updateSnackBarMessage('Something went wrong when trying to retrieve profiles.');
+    }
+  }, [limit, page, initialParams, setLoadingProfiles, delay, updateSnackBarMessage]);
 
   useEffect(() => {
     if (firstLoad.current && loadOnMount) {
