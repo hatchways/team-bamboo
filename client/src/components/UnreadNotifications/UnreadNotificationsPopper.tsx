bf0ff1a1b3bf72fec20804a70dc7adc67f0416d3
KookiKodes <kookikodes@gmail.com>
2022-02-06 15:55:59 -0800

    Added socket listeners for both when a new notification arises and when the notifications have been read

--- client/src/components/UnreadNotifications/UnreadNotificationsPopper.tsx
+++ client/src/components/UnreadNotifications/UnreadNotificationsPopper.tsx
@@ -1,10 +1,11 @@
-import type { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { Stack, Popper, Box, Typography, ClickAwayListener, Fade } from '@mui/material';
 import UnreadNotificationSkeleton from './UnreadNotificationSkeleton';
 import UnreadNotificationItem from './UnreadNotificationItem';
 import { useNotifications } from '../../context/useNotificationContext';
 import { useUnreadNotificationsPopper } from './context/useNotificationPopperContext';
 import { useStyles } from './hook/useStyles';
+import { useSocket } from '../../context/useSocketContext';
 
 const NoNotificationsText = () => (
   <Typography
@@ -25,8 +26,20 @@ const ErrorText = ({ children }: { children: string }) => {
 
 const UnreadNotificationsPopper = () => {
   const classes = useStyles();
-  const { matchNotifications } = useNotifications();
+  const { matchNotifications, loadNotifications } = useNotifications();
   const { anchorEl, isOpen, onClose } = useUnreadNotificationsPopper();
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('new-notification', loadNotifications);
+      socket.on('mark-notification-read', loadNotifications);
+      return () => {
+        socket.off('new-notification');
+        socket.off('mark-notification-read');
+      };
+    }
+  }, [socket, loadNotifications]);
 
   return (
     <Popper open={isOpen} anchorEl={anchorEl} placement="bottom" transition>
@@ -56,9 +69,9 @@ const UnreadNotificationsPopper = () => {
                         data.notifications.length === 0 ? (
                           <NoNotificationsText />
                         ) : (
-                          data.notifications.map(({ id, receivers, ...notification }) => (
-                            <UnreadNotificationItem key={id} {...notification} />
-                          ))
+                          data.notifications.map(({ id, notifyType, receivers, ...notification }) => {
+                            return <UnreadNotificationItem key={id} to={'#notification'} {...notification} />;
+                          })
                         ),
                     )}
                   </Stack>
